6e4bd263b47a8cb5fb37638013d764a7ec05a936
Alexander Kleemann <kleemannalexander@gmail.com>
2023-03-13 20:31:43 +0100

    Update workflow builder

--- Assets/Kenney Interface Sounds 1/description.md
+++ Assets/Kenney Interface Sounds 1/description.md
@@ -1,2 +1,16 @@
-# Floor Shelf
-I've got to admit that I'm not entirely sure what to call this. It's some sort of container for a blob sculpture with integrated lighting. Lightmap ready.
+# Kenney Interface Sounds #1
+This is a sound interface package from Kenney that includes hundreds of sounds for interfaces.
+
+[![Kenney Website](https://www.kenney.nl/data/img/logo.png)](https://www.kenney.nl)
+
+If you like this, please support Kenney by becoming a Patreon:
+
+[Patreon Link](https://www.patreon.com/kenney) (Patreon)
+
+[Donate Link](https://kenney.itch.io/kenney-donation) (Itch.io)
+
+Or you can buy his game assets package here:
+
+[![Kenney Game Assets](https://www.kenney.nl/data/img/allin1.png)](https://kenney.itch.io/kenney-game-assets)
+
+License is CC0.
--- main.py
+++ main.py
@@ -1,7 +1,64 @@
-import os
+import os, json
+from time import strftime, localtime
 
-print("Directory contents:")
-for f in os.listdir():
-    print(f)
-with open('test.json', 'w') as outfile:
-    outfile.write("This file was made with Github actions!")
+#This file indexes all the assets in the current repository
+
+repo = "A3D-Blacksmith/BM-Main"
+repo_manifest = []
+
+def readManifest(path):
+
+    file = "Assets/" + path + "/manifest.json"
+    f = open(file, "r")
+    content = json.loads(f.read())
+    return content
+
+def getFileTime(path):
+    
+    file = "Assets/" + path + "/Assets.zip"
+    return os.path.getmtime(file) #Unix epoch
+
+def getFileSize(path):
+
+    file = "Assets/" + path + "/Assets.zip"
+    file_stats = os.stat(file)
+    return(str(file_stats.st_size)) #Kilobytes
+
+def getDescription(path):
+
+    file = "Assets/" + path + "/description.md"
+    f = open(file, "r")
+    return str(f.read())
+
+def getThumbnailUrl(path):
+
+    if os.path.isfile("Assets/" + path + "/thumbnail.gif"):
+        return "Assets/" + path + "/thumbnail.gif"
+    elif os.path.isfile("Assets/" + path + "/thumbnail.jpg"):
+        return "Assets/" + path + "/thumbnail.jpg"
+    else:
+        return "Assets/" + path + "/thumbnail.png"
+
+for name in os.listdir("Assets"):
+
+    asset = {}
+
+    asset["name"] = name
+    asset["repo"] = repo
+    asset["manifest"] = readManifest(name)
+    asset["creator"] = asset["manifest"]["creator"]
+    asset["tags"] = asset["manifest"]["tags"]
+    asset["realname"] = asset["manifest"]["name"]
+    asset["category"] = asset["manifest"]["category"]
+    asset["date"] = getFileTime(name)
+    asset["size"] = getFileSize(name)
+    asset["thumbnail"] = getThumbnailUrl(name)
+    asset["description"] = getDescription(name)
+    
+
+    repo_manifest.append(asset)
+
+print(repo_manifest[0])
+
+with open('bm_repo_index.json', 'w') as outfile:
+    outfile.write(str(repo_manifest))
